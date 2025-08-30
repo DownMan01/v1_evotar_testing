68a2ceeee2b024b8079f2d8d66760eee14aa253c
J <98541904+DownMan01@users.noreply.github.com>
2025-08-30 21:20:43 +0800

    Update SecuritySettings.tsx

--- src/components/SecuritySettings.tsx
+++ src/components/SecuritySettings.tsx
@@ -73,7 +73,6 @@ export const SecuritySettings = () => {
                 <Button
                   onClick={() => setShowSetup(true)}
                   disabled={loading}
-                  className="w-full sm:w-auto"
                 >
                   Enable 2FA
                 </Button>
